953d32286b0cb620a23d522c2db17d9f84b2a97b
samcbehrens <sbehrens@smith.edu>
2014-12-02 20:01:48 -0500

    added bitalino sdk

--- AffectiveGameCore/src/InformationProcessing/DTWwork.java
+++ AffectiveGameCore/src/InformationProcessing/DTWwork.java
@@ -1,5 +1,14 @@
 package InformationProcessing;
-
+/**@author sambehrens
+ *
+ *Using FastDTW classes compare kinect faces and training faces 
+ *
+ */
 public class DTWwork {
-
+	
+	/*
+	 * constructor
+	 */
+	public DTWwork(){
+	}
 }
